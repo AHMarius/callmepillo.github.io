a9b34d52d272eeb4e5cbaa1d9d60657a30a5a853
callmepillo <125155530+callmepillo@users.noreply.github.com>
2025-08-24 12:16:25 +0300

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -30,20 +30,24 @@ window.onload = function() {
   });
 
   /* apply effect to image */
-  const profile = document.querySelectorAll("#profile .glow");
+  const profile = document.querySelectorAll("#profile .glow")[0];
 
-  profile[0].addEventListener("mousemove", handleMouseMove);
+  window.addEventListener("mousemove", handleMouseMove);
 
   function handleMouseMove(e) {
+    /*
     const rect = this.getBoundingClientRect();
     const mouseX = e.clientX - rect.left - rect.width / 2;
     const mouseY = e.clientY - rect.top - rect.height / 2;
+    */
+    const mouseX = e.clientX - window.screenLeft - window.innerWidth / 2;
+    const mouseY = e.clientY - window.screenTop - window.innerHeight / 2;
 
+    
     let angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
     angle = (angle + 360) % 360;
 
-    this.style.setProperty("--start", angle + 60);
+    profile.style.setProperty("--start", angle + 60);
   }
-
 };
 
